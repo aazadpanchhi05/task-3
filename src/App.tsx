948dfb5e96b6628bafd5b44338bd9b0b7b46c789
aazadpanchhi05 <darshiljadav12@gmail.com>
2021-09-08 10:03:33 +0530

    SCSS Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import './App.css';
+import './App.scss';
 import { Child } from './Child';
 import { Theme, ThemeContext } from './ThemeContext';
 
